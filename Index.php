fe8763858b73d03c9223517d72df112148695a57
Ch4m0rr0 <134741712+Ch4m0rr0@users.noreply.github.com>
2024-04-07 22:43:21 -0600

    Roperous1

---
+++ Index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>serviRepuestos</title>
+</head>
+<body>
+    <h1>Roperito</h1>
+<h5>Lorem ipsum dolor sit amet.</h5>
+
+
+    
+</body>
+</html>
